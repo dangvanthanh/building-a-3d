3ec6b6fb516491afe5008f067c73c8322287b294
Dang Van Thanh <dangvanthanh@dangthanh.org>
2021-08-06 20:20:00 +0700

    Add group object

--- src/components/HelloThree.vue
+++ src/components/HelloThree.vue
@@ -11,11 +11,26 @@ export default {
     const canvas = this.$refs.webgl
     const scene = new THREE.Scene()
 
+    // Group
+    const group = new THREE.Group()
+    group.scale.y = 2
+    group.rotation.y = 0.2
+    scene.add(group)
+
     // Cube
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const meterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-    const mesh = new THREE.Mesh(geometry, meterial)
-    scene.add(mesh)
+    const cubeRed = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }))
+    cubeRed.position.x = - 1.5
+    group.add(cubeRed)
+    const cubeGreen = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }))
+    cubeGreen.position.x = 0
+    group.add(cubeGreen)
+    const cubeBlue = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x0000ff }))
+    cubeBlue.position.x = 1.5
+    group.add(cubeBlue)
+
+    // Axes helpers
+    const axesHelper = new THREE.AxesHelper()
+    scene.add(axesHelper)
 
     // Sizes
     const sizes = {
@@ -26,12 +41,13 @@ export default {
     // Camera
     const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
     camera.position.z = 3
-    camera.position.x = 1
     scene.add(camera)
+
+    camera.lookAt(new THREE.Vector3(0, 0, 0))
 
     // Renderer
     const renderer = new THREE.WebGLRenderer({
-      canvas
+      canvas,
     })
 
     renderer.setSize(sizes.width, sizes.height)
